0e162ffe57cae156cc441fb79e72cd56ae560567
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-21 01:55:57 +0600

    Реализовал представление product_add_view

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from webapp.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Product, Category
 
 
 def products_view(request):
@@ -11,3 +11,17 @@ def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, template_name='product_view.html', context={'product': product})
 
+
+def product_add_view(request):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, template_name='product_create.html', context={'categories': categories})
+    elif request.method == "POST":
+        product = Product.objects.create(
+            title=request.POST.get('title'),
+            price=request.POST.get('price'),
+            image=request.POST.get('image'),
+            description=request.POST.get('description'),
+            category_id=request.POST.get('category_id')
+        )
+        return redirect("product_view", pk=product.pk)
