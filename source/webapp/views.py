0f32c8dfb66e7d3eaf577723d8371ad062d645d4
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-21 00:25:58 +0600

    Реализовано представление products_view

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from webapp.models import Product
 
-# Create your views here.
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, template_name='products_list.html', context={'products': products})
