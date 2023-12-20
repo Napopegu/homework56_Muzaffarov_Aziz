bbf8ea8e9c3f92cb47ba546bcbadc71e7fb87830
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-21 01:09:33 +0600

    Реализовал представление product_view

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
 
 
 def products_view(request):
     products = Product.objects.all()
     return render(request, template_name='products_list.html', context={'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, template_name='product_view.html', context={'product': product})
+
